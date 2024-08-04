1157a35798efc2692f9e90ce6b44d89a0cdb5b2d
Hrithik Agarwal <hrithikinfinite@gmail.com>
2024-08-04 23:04:33 +0530

    added logging

--- app/main.js
+++ app/main.js
@@ -1,4 +1,5 @@
 function matchPattern(inputLine, pattern) {
+  console.log(`pattern inside fcn =${pattern}, inputLine=${inputLine}`);
   if (pattern.length === 1) {
     return inputLine.includes(pattern);
   } else if (pattern === "\\d") {
@@ -13,14 +14,9 @@ function main() {
   const inputLine = require("fs").readFileSync(0, "utf-8").trim();
   console.log(`Pattern =>${pattern} Input Line =>${inputLine}`);
   if (process.argv[2] !== "-E") {
-    console.log("Expected first argument to be '-E'");
     process.exit(1);
   }
 
-  // You can use print statements as follows for debugging, they'll be visible when running tests.
-  console.log("Logs from your program will appear here");
-
-  // Uncomment this block to pass the first stage
   if (matchPattern(inputLine, pattern)) {
     process.exit(0);
   } else {
